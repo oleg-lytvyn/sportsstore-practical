db33d8ea2a96b94810fe7c28e903bca5ee25bc5f
oleh <olehlytvyn.personal@gmail.com>
2024-11-06 17:13:16 +0000

    Testing The new code uses Express and enables support to decode JSON and form-encoded content, using the json and urlencoded methods

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello, SportsStore");
+const http_1 = require("http");
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const port = 5000;
+const expressApp = (0, express_1.default)();
+expressApp.use((0, helmet_1.default)());
+expressApp.use(express_1.default.json());
+expressApp.use(express_1.default.urlencoded({ extended: true }));
+expressApp.get("/", (req, resp) => {
+    resp.send("Hello, SportsStore");
+});
+const server = (0, http_1.createServer)(expressApp);
+server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello, SportsStore");
+import { createServer } from "http";
+import express, { Express } from "express";
+import helmet from "helmet";
+const port = 5000;
+const expressApp: Express = express();
+expressApp.use(helmet());
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({ extended: true }))
+expressApp.get("/", (req, resp) => {
+    resp.send("Hello, SportsStore");
+})
+const server = createServer(expressApp);
+server.listen(port,
+    () => console.log(`HTTP Server listening on port ${port}`));
