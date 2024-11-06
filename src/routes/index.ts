682fb85f76b7af95669997953036209ea07d1b20
oleh <olehlytvyn.personal@gmail.com>
2024-11-06 20:34:10 +0000

    Previous commit changes

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { Express } from "express";
+import { createCatalogRoutes } from "./catalog";
+export const createRoutes = (app: Express) => {
+    createCatalogRoutes(app);
+}
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import { createServer } from "http";
 import express, { Express } from "express";
 import helmet from "helmet";
 import { getConfig } from "./config";
+import { createRoutes } from "./routes";
 const port = getConfig("http:port", 5000);
 const expressApp: Express = express();
 expressApp.use(helmet());
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }))
-expressApp.get("/", (req, resp) => {
-    resp.send("Hello, SportsStore");
-})
+// expressApp.get("/", (req, resp) => {
+//     resp.send("Hello, SportsStore");
+// })
+createRoutes(expressApp);
 const server = createServer(expressApp);
 server.listen(port,
     () => console.log(`HTTP Server listening on port ${port}`));
