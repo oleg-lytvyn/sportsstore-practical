981499d00a6597dc4f5da02c490b4760050004da
oleh <olehlytvyn.personal@gmail.com>
2024-11-06 21:07:49 +0000

    Access to localhost was denied-You don't have authorization to view this page. HTTP ERROR 403

--- dist/server.js
+++ dist/server.js
@@ -8,8 +8,10 @@ const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
 const config_1 = require("./config");
 const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
 const port = (0, config_1.getConfig)("http:port", 5000);
 const expressApp = (0, express_1.default)();
+expressApp.use((0, cors_1.default)());
 expressApp.use((0, helmet_1.default)());
 expressApp.use(express_1.default.json());
 expressApp.use(express_1.default.urlencoded({ extended: true }));
@@ -18,4 +20,8 @@ expressApp.use(express_1.default.urlencoded({ extended: true }));
 // })
 (0, routes_1.createRoutes)(expressApp);
 const server = (0, http_1.createServer)(expressApp);
-server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+// server.listen(port,
+//     () => console.log(`HTTP Server listening on port ${port}`));
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import express, { Express } from "express";
 import helmet from "helmet";
 import { getConfig } from "./config";
 import { createRoutes } from "./routes";
+import cors from "cors";
 const port = getConfig("http:port", 5000);
 const expressApp: Express = express();
+expressApp.use(cors());
 expressApp.use(helmet());
 expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }))
@@ -13,5 +15,9 @@ expressApp.use(express.urlencoded({ extended: true }))
 // })
 createRoutes(expressApp);
 const server = createServer(expressApp);
-server.listen(port,
-    () => console.log(`HTTP Server listening on port ${port}`));
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+    // server.listen(port,
+    //     () => console.log(`HTTP Server listening on port ${port}`));
