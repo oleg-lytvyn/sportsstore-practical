45466dfc077c2b6e532fc554a4e14f25eb744bf9
oleh <olehlytvyn.personal@gmail.com>
2024-11-07 15:49:10 +0000

    feat(routes, templates): add catalog route and template for displaying product list

--- src/routes/catalog.ts
+++ src/routes/catalog.ts
@@ -1,14 +1,14 @@
 import { Express } from "express";
+import { catalog_repository } from "../data";
 export const createCatalogRoutes = (app: Express) => {
-    app.get("/", (req, resp) => {
-        // resp.send("Hello, SportsStore Route");
-        resp.render("index");
-    });
-    app.get("/err", (req, resp) => {
-        throw new Error("Something bad happened");
-    });
-
-    app.get("/asyncerr", async (req, resp) => {
-        throw new Error("Something bad happened asynchronously");
-    });
+    app.get("/", async (req, resp) => {
+        const products = await catalog_repository.getProducts();
+        resp.render("index", { products });
+    })
+    // app.get("/err", (req, resp) => {
+    //     throw new Error ("Something bad happened");
+    // });
+    // app.get("/asyncerr", async (req, resp) => {
+    //     throw new Error ("Something bad happened asynchronously");
+    // });
 }
